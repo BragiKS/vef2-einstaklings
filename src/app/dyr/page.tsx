160f842f2f645bb0555e88347d11afc9fc91bd3e
BragiKS <bks9@hi.is>
2024-03-29 09:42:40 +0000

    byrting gagna virkar nuna

--- src/app/dyr/page.tsx
+++ src/app/dyr/page.tsx
@@ -12,13 +12,20 @@ async function getDyr() {
   return res.json();
 }
 
+interface DyrPackage {
+  result: Animal[];
+}
+
+// Your page component
 export default async function Dyr() {
-  const data = await getDyr();
+  const dataPack: DyrPackage = await getDyr();
+  const data = dataPack.result;
+
   return (
     <div>
       <h1>Halló dýr</h1>
       <div className="cards">
-        {data.map((animal: Animal) => (
+        {data.map((animal) => (
           <div key={animal.species}>
             <h3>{animal.species}</h3>
             <p>{animal.type}</p>
@@ -26,7 +33,7 @@ export default async function Dyr() {
               Stærð:{" "}
               {animal.size_hi === animal.size_lo
                 ? animal.size_hi
-                : animal.size_lo + "-" + animal.size_hi}
+                : `${animal.size_lo}-${animal.size_hi}`}
             </p>
           </div>
         ))}
